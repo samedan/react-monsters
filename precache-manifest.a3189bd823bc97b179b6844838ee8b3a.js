ca16ce6e34fb9c2358222a2b769612d3676fbc01
samedan <same.dan@gmail.com>
2019-10-11 16:08:51 +0200

    Updates

---
+++ precache-manifest.a3189bd823bc97b179b6844838ee8b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce75efa455b5aa1af5336ca885eb32e6",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "9161a284f8568d17682a",
+    "url": "/react-monsters/static/css/main.42cda3a4.chunk.css"
+  },
+  {
+    "revision": "6db1f5f7c32ced106e27",
+    "url": "/react-monsters/static/js/2.efe1e767.chunk.js"
+  },
+  {
+    "revision": "9161a284f8568d17682a",
+    "url": "/react-monsters/static/js/main.5ae92544.chunk.js"
+  },
+  {
+    "revision": "d7659c7658d790b2b799",
+    "url": "/react-monsters/static/js/runtime-main.015cfbb6.js"
+  }
+]);
